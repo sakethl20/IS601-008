1083fd2efbdb16db1f28a49456aebf61c745c97a
Saketh Lakshmanan <saketh_lakshmanan@yahoo.com>
2023-02-08 23:40:07 -0500

    changed file structure

---
+++ Exercises/temp2.py
@@ -0,0 +1,4 @@
+import tempCodeRunnerFile as p
+
+print(p.addNum(3,4))
+print(p.findTriangle(7,8))
---
+++ Exercises/tempCodeRunnerFile.py
@@ -0,0 +1,43 @@
+#Saketh Lakshmanan
+#sl778
+#Tuesday Feb 7
+
+# x = 2
+# y = 'Hello'
+# z = 3.5
+
+# print (type(x))
+# print (type(y))
+# print (type(z))
+
+# num1 = int(input("Enter your first number: "))
+# num2 = int(input("Enter your second number: "))
+
+# if num1 > num2:
+#     print(num1)    
+# elif num2 > num1:
+#     print(num2)
+# else:
+#     print("None of the above is true.")   
+
+# h = 0
+
+# while h < 10:
+#     h+=1
+#     print(h)
+    
+# j = "Greetings"
+
+# for i in j:
+#     print(i)
+
+def addNum(a,b):
+    return a+b
+
+#print(addNum(2,3))
+
+
+def findTriangle(a,b):
+    return a*b*(0.5)
+
+#print(findTriangle(4,5))
